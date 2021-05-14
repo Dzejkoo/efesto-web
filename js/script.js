efd972fe865730ed6391b1df1c32ed0bbf35dac5
Jakub Jóźwiak <jakub.lazy@gmail.com>
2021-05-14 21:38:29 +0200

    mobile menu

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 let nav_button = document.querySelector('.nav__button');
 let nav_list = document.querySelector('.nav__list');
+let body = document.querySelector('body');
 
 nav_button.addEventListener('click', () => {
     nav_button.classList.toggle('nav__button--open')
     nav_list.classList.toggle('nav__list--open')
+    nav_list.classList.add('nav__list--visible')
+    body.classList.toggle('open-menu')
 })
